db1c5b2aeb222ad8485de7d833f7ce0f849a8d8b
Gabriel Cunha <gabriel.cunha@serverwin.tsp>
2022-03-14 14:34:46 -0300

    Added vercel json file

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,9 @@ import { Tweet } from "./components/Tweet"
 import { AppRoutes } from "./Routes"
 
 function App() {
-  const [tweets, setTweets] = useState<string[]>([
-    "Tweet 1",
-    "Tweet 2",
-    "Tweet 3",
-  ])
-
-  function createTweet(){
-    setTweets([...tweets, "Tweet 5"])
-  }
 
   return (
     <AppRoutes />
-    /*<div>
-      {tweets.map( tweet => {
-        return <Tweet text={tweet} />
-      })}
-
-      <button
-        onClick={createTweet}
-        style={
-          {
-            backgroundColor: "#223487",
-            border: 0,
-            padding: '5px',
-            color: '#fff'
-          }
-        }
-      >
-        Adicionar tweet
-      </button>
-    </div>*/
-    
   )
 }
 
